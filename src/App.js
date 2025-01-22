f6b3c903ab08cece11518cb8df0c48a587f5760a
liztrenholme <liztrenholme@gmail.com>
2025-01-21 23:27:47 -0500

    adds iframe and debugging logs for local and session storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-import Main from './components/main';
+// import Main from './components/main';
 
 function App() {
+  useEffect(() => {
+    window.sessionStorage.setItem('foo', 'bar');
+    window.sessionStorage.setItem('item1', 'cat');
+    window.localStorage.setItem('item2', 'dog');
+    window.localStorage.setItem('item3', 'fish');
+    window.addEventListener('message', function(event){
+      console.log('incoming from iframe?', event, event.data);
+    });
+
+    // Cleanup the listener when the component unmounts
+    return () => {
+      window.removeEventListener('message', () => {});
+    };
+  }, []); // Empty dependency array means this effect runs once on mount
+  console.log('from parent...', window.localStorage.getItem('item1'));
   return (
     <div className="App">
-      <Main />
+      <iframe 
+      // sandbox='allow-storage-access-by-user-activation allow-scripts allow-same-origin'
+        width={window.innerWidth - 100} height={window.innerHeight - 100} title='iframe example' src="http://localhost:3001/pokemon-search#token=123456789" />
     </div>
   );
 }
